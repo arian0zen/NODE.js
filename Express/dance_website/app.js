6f8d62fc6e0e250f9a2d8057fc35cb504e2e16ff
Arian Shaikh <ariansk90@gmail.com>
2022-07-06 08:42:56 +0530

    morning babe

--- Express/dance_website/app.js
+++ Express/dance_website/app.js
@@ -3,7 +3,7 @@ const path  = require("path");
 // const fs = require("fs");
 const { allowedNodeEnvironmentFlags } = require("process");
 const app = express();
-const port = 8000;
+const port = 80;
 
 
 //express
